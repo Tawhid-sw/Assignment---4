85a8489bd31ef04c3c318a3ca70a759865f967b6
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-07-09 14:42:48 +0600

    implement notFound and cros origin

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,18 @@ import express, { type Application } from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./modules/auth/auth.routes";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
+import cors from "cors";
+import { config } from "./config";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.APP_URL,
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +24,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRouter);
+
+app.use(notFound);
 
 app.use(globalErrorHandler);
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,7 @@ export const config = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
+  APP_URL: process.env.APP_URL,
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
