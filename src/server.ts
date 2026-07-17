54a75bc7cee38249d2863c781aee99b9e40fb7dd
Tawhid-sw <tawhidsarkar684@gnail.com>
2026-07-18 01:37:44 +0600

    another approach

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,20 @@ import app from "./app";
 import { config } from "./config";
 import { prisma } from "./lib/prisma";
 
-export default app;
+const PORT = config.PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = config.PORT || 3000;
-
-  async function main() {
-    try {
-      await prisma.$connect();
-      console.log("Connected to the database successfully.");
-      app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-      });
-    } catch (error) {
-      console.error("Error starting the server:", error);
-      await prisma.$disconnect();
-      process.exit(1);
-    }
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
   }
-
-  main();
 }
+
+main();
